0fc933f50fa99e41b3f9adbe7b96eee18e80c3ee
salmansaheer <salmanvk23yr@gmail.com>
2022-12-21 20:56:42 +0530

    changes in settings and user urls(home page)

--- Hospitail_Union/Hospitail_Union/settings.py
+++ Hospitail_Union/Hospitail_Union/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
--- Hospitail_Union/users/views.py
+++ Hospitail_Union/users/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'users/home.html')
 # Create your views here.
